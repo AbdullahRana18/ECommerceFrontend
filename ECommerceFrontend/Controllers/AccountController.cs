e6e831e8f9106ba632ec418c7504204b74a95ca3
Abdullah Rana <the.abdullah.1829@gmail.com>
2025-10-07 19:50:43 +0500

    fix ui and Footer

--- ECommerceFrontend/Controllers/AccountController.cs
+++ ECommerceFrontend/Controllers/AccountController.cs
@@ -1,15 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
 
 namespace ECommerceFrontend.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IHttpClientFactory clientFactory;
+        public AccountController(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+        }
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(String Email , string Password)
+        {
+            var client = clientFactory.CreateClient("ECommerceAPI");
+            var loginData = new { Email = Email, Password = Password };
+            var content = new StringContent(
+               JsonSerializer.Serialize(loginData),
+               Encoding.UTF8,
+               "application/json"
+               );
+            var response = await client.PostAsync("User/login", content);
 
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+
+               
+                HttpContext.Session.SetString("JWToken", result);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid email or password.";
+            return View();
+        }
         [HttpGet]
         public IActionResult Signup()
         {
--- ECommerceFrontend/obj/Debug/net9.0/ECommerceFrontend.AssemblyInfo.cs
+++ ECommerceFrontend/obj/Debug/net9.0/ECommerceFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0feb9744dbb5570e02a79eb1f785d7181d85516b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364beb14705f721d7ae5b1f85636c1287da375eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
